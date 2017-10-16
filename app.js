2c51d8929318c62977a50edec9b79d2c26f8b662
Abhijeet <s530670@nwmissouri.edu>
2017-10-15 19:29:02 -0500

    Mail functionality added

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./src/backend/routes/index');
-var users = require('./src/backend/routes/users');
 
 var app = express();
 
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/conctus', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var api_key = 'key-58373f910b52cb87142f833f3b646658';
+var domain = 'sandbox2f6c24e87b0f4ad6aa021d50908cd8cb.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+ 
+
 
 /* GET home page. */
 router.get(['/','/our-vision','/contactus','/learning-paths'], function(req, res, next) {
   res.render('index.html', { title: 'Express' });
 });
 
+router.post('/contactUs',function(req,res){
+  var data = {
+    from: 'Excited User <abhijitagrawal09@gmail.com>',
+    to: req.body.email,
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomness!'
+  };
+   
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+    res.send({status:400 , message:"Thanks for contacting us"});
+  });
+  
+})
 module.exports = router;
